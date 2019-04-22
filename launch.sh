3bf30d3c311b9a5d16ffe03f03ca644f90a3976f
Chase <thundr@protonmail.ch>
2019-04-22 12:27:10 -0700

    add dns changes

--- launch.sh
+++ launch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-docker-compose run --rm openvpn ovpn_genconfig -u udp://midasarray.thundr.me -n dns
+# DNS_IP=$(docker inspect --format '{{ .NetworkSettings.Networks.midas_net.IPAddress }}' $(docker ps -aqf "name=dns"))
+DNS_IP="172.75.0.2"
+docker-compose run --rm openvpn ovpn_genconfig -u udp://midasarray.thundr.me -n $DNS_IP
 docker-compose run --rm openvpn ovpn_initpki
 docker-compose up
 
