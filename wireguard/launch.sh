c0d7d12b376c6c67213fa9b8e62bc46c15e84a0f
Chase <thundr@protonmail.ch>
2019-05-15 09:46:56 -0700

    hardcode wireguard interface for now

--- wireguard/launch.sh
+++ wireguard/launch.sh
@@ -3,12 +3,12 @@
 set -e
 
 echo "$(date): Starting Wireguard"
-wg-quick up $interface
+wg-quick up wgnet0
 
 # Handle shutdown behavior
 finish () {
     echo "$(date): Shutting down Wireguard"
-    wg-quick down $interface
+    wg-quick down wgnet0
     exit 0
 }
 
