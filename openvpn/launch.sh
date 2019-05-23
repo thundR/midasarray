0d3145f919eddcc21d53c53babbfe95a4458c07d
Chase <thundr@protonmail.ch>
2019-05-23 09:23:22 -0700

    remove test, fix route

--- openvpn/launch.sh
+++ openvpn/launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # DNS_IP=$(docker inspect --format '{{ .NetworkSettings.Networks.midas_net.IPAddress }}' $(docker ps -aqf "name=dns"))
 DNS_IP="172.75.0.2"
-docker-compose run --rm openvpn ovpn_genconfig -p 'route 172.25.0.0 255.255.0.0' -u udp://midasarray.thundr.me -n $DNS_IP
+docker-compose run --rm openvpn ovpn_genconfig -p 'route 172.75.0.0 255.255.0.0' -u udp://midasarray.thundr.me -n $DNS_IP
 docker-compose run --rm openvpn ovpn_initpki
