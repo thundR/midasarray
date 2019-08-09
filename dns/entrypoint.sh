3accbe6c2430dbc77eeb8901ea02561266325d9a
thundR <thundr@protonmail.ch>
2019-08-09 01:20:42 +0000

    misc changes

--- dns/entrypoint.sh
+++ dns/entrypoint.sh
@@ -3,6 +3,6 @@
 set -e
 
 echo "$(date): Starting dnscrypt-proxy"
-ip route add 192.168.255.0/24 via 172.75.0.3 dev eth0
+ip route add 192.168.255.0/24 via 172.16.0.3 dev eth0
 /opt/dnscrypt-proxy
 
